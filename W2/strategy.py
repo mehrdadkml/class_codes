594e3076605d0e7e038447696c1447d3b11c8911
Teghfo <teghfo@gmail.com>
2021-03-05 10:26:18 +0330

    update strategy

--- W2/strategy.py
+++ W2/strategy.py
@@ -2,8 +2,8 @@ from abc import ABC, abstractmethod
 
 
 class Order:
-    def __init__(self, name , phone, cart, prom=None):
-        self.name  = name
+    def __init__(self, name, phone, cart, prom=None):
+        self.name = name
         self.phone = phone
         self.cart = cart
         self.prom = prom
@@ -15,7 +15,16 @@ class Order:
         return total_pay
 
     def due(self):
-        return self.total() - self.prom.discount(self.cart)
+        if self.prom:
+            return self.total() - self.prom.discount(self.cart)
+        else:
+            return self.total()
+
+    def mizan_takhfif(self):
+        if self.prom:
+            return self.prom.discount(self.cart)
+        else:
+            return 0
 
 
 class Item:
@@ -27,7 +36,6 @@ class Item:
     @property
     def get_total_price(self):
         return self.price * self.qty
-
 
 
 class Promotion(ABC):
@@ -49,10 +57,10 @@ class MoshtarianSetare(Promotion):
 
 class LargeOrder(Promotion):
     '''moshtariane zuad bekhar man 7 darsad takhfif'''
-    
+
     def discount(self, cart):
         takhfif = 0
-        if sum(item.qty for item in cart) >=50:
+        if sum(item.qty for item in cart) >= 50:
             for item in cart:
                 takhfif += item.get_total_price
             return takhfif * 0.07
@@ -61,21 +69,27 @@ class LargeOrder(Promotion):
 
 class MoshtarianMotadJens(Promotion):
     '''moshtariane eshghe ye jens(vaghti 20ta va bishtar) 5 darsad takhfif ru un jense'''
-    
+
     def discount(self, cart):
         takhfif = 0
         for item in cart:
-            if item.qty >=20:
+            if item.qty >= 20:
                 takhfif += (item.get_total_price * 0.05)
         return takhfif
-
 
 
 sib = Item('sib', 1000, 10)
 port = Item('port', 2500, 10)
 mowz = Item('mowz', 150, 21)
 
-order = Order('ashkan', 912222222, [sib, port, mowz], prom=MoshtarianMotadJens())
+order = Order('ashkan', 912222222, [
+              sib, port, mowz], prom=MoshtarianMotadJens())
 
+orderAkbarKasif = Order('akbar', 91222, [port])
 
+print("akbar: ", orderAkbarKasif.due())
 print(f"ghabl takhfif {order.total()} bad takhfif {order.due()}")
+
+
+print("akbar: ", orderAkbarKasif.mizan_takhfif())
+print("shaghayegh: ", order.mizan_takhfif())
